287a7dc6237ae2f9ed6bcf1ce1438ae2d296bfa0
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-09-22 03:20:14 +0530

    learning hooks and create countet button

--- react-blog/src/ClickEventAndFunctionCall.jsx
+++ react-blog/src/ClickEventAndFunctionCall.jsx
@@ -3,9 +3,29 @@ function Click(){
         alert("function called")
     }
 
+    // const fruit = (name)=>{
+    //      alert(name);
+    // }
     const fruit = (name)=>{
-         alert(name);
+        <h1>{name}</h1>
     }
+
+
+// import { useState } from "react";  // yaha me usestate use kar rha  hu 
+
+// function Click() {
+//     const [fruitName, setFruitName] = useState("");  // state banayi
+
+//     function callFun() {
+//         alert("function called");
+//     }
+
+//     const fruit = (name) => {
+//         setFruitName(name);   // state update kiya
+//     };
+
+
+
     return (
         <div>
             <h1>Event and function call</h1>
@@ -13,6 +33,7 @@ function Click(){
             <button onClick={()=>fruit("banana")}>Banana</button>
             <button onClick={()=>fruit("apple")}>Apple</button>
             {/* react me function ko call krne k liye uski diffination chahiye hoti hai na ki uski call like js me hum karte hai wese   */}
+            {/* <h2>{fruitName}</h2>   ye chije use state k sath use hoti hai yaha me pahle hi try kr rah tha */}
         </div>
     )
 }
---
+++ react-blog/src/StateInReactjs.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Counter from "./Counter";
+
+function State(){
+    const [fruit, setFruit] = useState("Apple");
+    const handleFruit=()=>{
+        setFruit("banana");
+    }
+ 
+        
+    
+    return (
+        <div>
+            <h1>State in React Js</h1>
+            <h1>{fruit}</h1>
+            <button onClick={handleFruit}>Change fruit Name</button>
+            <Counter/>
+        </div>
+    )
+
+}
+export default State;
