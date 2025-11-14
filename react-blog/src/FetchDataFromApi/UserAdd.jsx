f8d2f213340570709102476c54112fc3f887eee7
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-11-14 23:25:51 +0530

    integrated post api

--- react-blog/src/FetchDataFromApi/UserAdd.jsx
+++ react-blog/src/FetchDataFromApi/UserAdd.jsx
@@ -1,12 +1,34 @@
-
+import { useState } from "react";
 
 export default function UserAdd() {
+    const [name,setName]=useState('');
+    const [age,setAge]=useState('');
+    const [email,setEmail]=useState('');
+
+    const creatUser= async()=>{
+        console.log(name,age,email);
+        const url="http://localhost:3000/users";
+        let response = await fetch(url,{
+            method:'Post',
+            body:JSON.stringify({name,email,age})
+        }) ;
+        response= await response.json();
+        if(response){
+            alert("new user added ")
+        }
+    }
+
+
   return (
-    <div>
+    <div style={{textAlign:"center"}}>
       <h1>Add New User</h1>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+      <input type="text" onChange={(event)=>setName(event.target.value)} placeholder="enter name" />
+      <br /><br />
+      <input type="text" onChange={(event)=>setAge(event.target.value)} placeholder="enter age"/>
+      <br /><br />
+      <input type="email" onChange={(event)=>setEmail(event.target.value)} placeholder="enter email"/>
+      <br /><br />
+      <button onClick={creatUser}>Add New</button>
     </div>
   )
 }
