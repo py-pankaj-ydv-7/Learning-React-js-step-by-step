032c898f83fc8ba0dba2f366b75e96098f95a659
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-11-16 23:42:00 +0530

    integration api for delete method

--- react-blog/src/FetchDataFromApi/UserList.jsx
+++ react-blog/src/FetchDataFromApi/UserList.jsx
@@ -4,22 +4,34 @@ import "../index.css"
 export default function UserList() {
   const [userData,setUserData]=useState([]);
   // yaha ye state me loader dikhane k liye use kr rh hu .
-  const [ loading,setLoading]=useState(false)
+  const [ loading,setLoading]=useState(false);
+  const url ="http://localhost:3000/users";
   useEffect(()=>{
     setLoading(true);
     getUserData();
     
   },[])
   const getUserData = async()=>{
-    const url ="http://localhost:3000/users";
+    // const url ="http://localhost:3000/users"; // mene bhar likh diya hai stats k pass ise 
     let response = await fetch(url);
     response = await response.json();
     console.log(response);
     setUserData(response)
     setLoading(false)
-    
+  }
+
+  const deleteUser= async(id)=>{
+    let response = await fetch(url+"/"+id,{
+      method:"delete",
+    })
+    response = await response.json();
+    if(response){
+      alert("record deleted")
+    }
     
   }
+
+
   return (
     <div>
         <h1>Make Routes and Pages for Add User and User List UI</h1>
@@ -27,6 +39,7 @@ export default function UserList() {
               <li>Name</li>
               <li>Age</li>
               <li>Email</li>
+              <li>Action</li>
             </ul>
         {
           !loading?
@@ -36,6 +49,7 @@ export default function UserList() {
               <li>{user.name}</li>
               <li>{user.age}</li>
               <li>{user.email}</li>
+              <li><button onClick={()=>deleteUser(user.id)}>Button</button></li>
             </ul>
           ))
           :<h1>Loading data...</h1>
